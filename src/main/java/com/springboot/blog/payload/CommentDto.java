4edb06e5d2e8aa5672624429209509f2004945ed
ProboticsX <jainshubham0403@gmail.com>
2023-05-19 16:21:50 -0400

    Added validations

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,6 +2,9 @@ package com.springboot.blog.payload;
 
 import com.springboot.blog.entity.Post;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,12 @@ import lombok.NoArgsConstructor;
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 10, message = "Comment body should be of 10 characters minimum")
     private String body;
 }
